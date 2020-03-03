d2cd1bce643e4a29c6aee45f808e1b5d1c63c302
Drozdz.Magdalena <215707@edu.p.lodz.pl>
2020-03-03 19:02:34 +0100

    reszta testow

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -8,7 +8,7 @@ public class TailsGenerator {
     public List<String> tails(String value) {
 
         List<String> lista= new ArrayList<String>();
-        for(int i=0 ; i < value.length() ; i++){
+        for(int i=0 ; i <= value.length() ; i++){
             lista.add(i,value.substring(i,value.length()));
         }
         return lista;
